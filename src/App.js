c10d28c262656d4e736f0faa1b0b054c99778e8f
Eduardo <eduardom1997@hotmail.com>
2022-11-25 22:39:29 -0600

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    let date = new Date();
+    let options1 = {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZone: "America/Mexico_City",
+        timeZoneName: "long",
+    };
+    let options2 = {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZone: "Asia/Qatar",
+        timeZoneName: "long",
+    };
+
+    const element = (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hora</h1>
+                <h2>
+                    Son las{" "}
+                    {new Intl.DateTimeFormat("es-mx", options1).format(date)} en
+                    CDMX
+                </h2>
+                <h2>
+                    Son las{" "}
+                    {new Intl.DateTimeFormat("es-mx", options2).format(date)} en
+                    QATAR
+                </h2>
+            </header>
+        </div>
+    );
+
+    root.render(element);
 }
 
 export default App;
